24c27290370db0f1f2a2999072485b90477deb6c
Krystal <krystalmorel@gmail.com>
2025-07-16 15:32:42 -0400

    help

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './componets/Sidebar'
+import Player from './componets/Player'
+import Display from './componets/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
+
+  const {audioRef} = useContext(PlayerContext)
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+        <Display />
       </div>
+      <Player />
+      <audio ref={audioRef} preload='auto'></audio>
       
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+    <PlayerContextProvider>
+      <App />
+    </PlayerContextProvider>
+    </BrowserRouter>
+    
+  </React.StrictMode>,
 )
